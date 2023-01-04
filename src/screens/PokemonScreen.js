076e210dfd06e491f6a232a3b97b6adaed822038
tib.songsakul <osacn_sataporn.songsakul@krungthai.com>
2023-01-04 18:08:31 +0700

    forgot to push

---
+++ src/screens/PokemonScreen.js
@@ -0,0 +1,10 @@
+import Details from './../components/Details'
+
+const PokemonScreen = ({ navigation, route }) => {
+
+  return (
+      <Details navigation={navigation} route={route}/>
+      )
+}
+
+export default PokemonScreen
